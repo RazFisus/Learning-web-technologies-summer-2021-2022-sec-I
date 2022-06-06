adf0fa9100a76d8088643ff192ae8c295827fd84
RazFisus <102638923+RazFisus@users.noreply.github.com>
2022-06-06 13:23:59 +0600

    Delete (20-42735-2)AT1.php

--- (20-42735-2)AT1.php
+++
@@ -1,9 +0,0 @@
-<?php
-function rect_area($length = 3, $width = 4) //function has declared
-{
-    $area = $length * $width;
-    echo "Area Of Rectangle with length " . $length . " & width " . $width . " is " . $area ;
-}
-rect_area(); // function has been called.
-
-?>
