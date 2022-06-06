5aaf3cfee06dc1b81274e08a79232b23abbba6e4
RazFisus <102638923+RazFisus@users.noreply.github.com>
2022-06-06 13:27:05 +0600

    Add files via upload

---
+++ (20-42735-1)AT1.php
@@ -0,0 +1,9 @@
+<?php
+function rect_area($length = 3, $width = 4) //function has declared
+{
+    $area = $length * $width;
+    echo "Area Of Rectangle with length " . $length . " & width " . $width . " is " . $area ;
+}
+rect_area(); // function has been called.
+
+?>
---
+++ (20-42735-1)AT2.php
@@ -0,0 +1,8 @@
+
+<?php
+$price =80;
+$vat = 1.16;
+$vprice = $price*$vat;
+echo "Normal Price $price<br>";
+echo "Vat Added Price $vprice";
+?>
